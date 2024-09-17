b6233d9e42a11254b31213e50a7f1ab5bdb86a6f
angel <angelcomu04@gmail.com>
2024-09-17 20:24:30 +0200

    semaforos y fondo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SemaforoComponent } from './components/semaforo/semaforo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SemaforoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/semaforo/semaforo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-semaforo',
+  templateUrl: './semaforo.component.html',
+  styleUrls: ['./semaforo.component.css']
+})
+export class SemaforoComponent {
+
+}
